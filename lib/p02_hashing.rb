4fa0f38cc8dae339910929c070853025a71cb4bd
Bruk Argaw <bkargaw@gmail.com>
2017-06-22 09:10:09 -0700

    finished hashing

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -1,14 +1,25 @@
+require 'byebug'
 class Fixnum
   # Fixnum#hash already implemented for you
 end
 
 class Array
   def hash
+    hash = 0
+    each_with_index do |el, idx|
+      hash += el.hash * idx
+    end
+    hash
   end
 end
 
 class String
   def hash
+    hash = 0
+    each_char.with_index do |char, idx|
+      hash += char.ord.hash * idx
+    end
+    hash
   end
 end
 
@@ -16,6 +27,11 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash = 0
+    each do |key, value|
+      hash += key.hash + value.hash
+    end
+
+    hash
   end
 end
