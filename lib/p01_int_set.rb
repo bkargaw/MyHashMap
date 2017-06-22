d4f1a6ee86b7dfa0407ec6367f62fb9c2e0fc254
Bruk Argaw <bkargaw@gmail.com>
2017-06-21 19:29:09 -0700

    finished set

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -1,3 +1,4 @@
+require('byebug')
 class MaxIntSet
   attr_reader :store, :max
   def initialize(max)
@@ -31,23 +32,30 @@ end
 
 
 class IntSet
+  attr_reader :store
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    return false if include?(num)
+    self[num].push(num)
   end
 
   def remove(num)
+    self[num].delete_if { |e| e == num } if include?(num)
   end
 
   def include?(num)
+    self[num].each { |el| return true if el == num }
+    false
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    store[num % num_buckets]
   end
 
   def num_buckets
@@ -56,7 +64,7 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -64,18 +72,29 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if num_buckets == count
+    return false if include?(num)
+    self[num].push(num)
+    @count += 1
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete_if { |e| e == num }
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].each { |el| return true if el == num }
+    false
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    store[num % num_buckets]
   end
 
   def num_buckets
@@ -83,5 +102,10 @@ class ResizingIntSet
   end
 
   def resize!
+    new_store = ResizingIntSet.new(num_buckets * 2)
+    @store.each do |array|
+      array.each { |num| new_store.insert(num) }
+    end
+    @store = new_store.store
   end
 end
